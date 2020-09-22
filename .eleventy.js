1c0a79f15833a64477992edfb7a0d25fe8d9a291
Christina Dan <cdan@socialnetconnect.com>
2020-09-22 00:05:36 -0500

    Add: eleventy dependency

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const CleanCSS = require('clean-css');
 
 module.exports = function (eleventyConfig) {
@@ -11,10 +12,21 @@ module.exports = function (eleventyConfig) {
     dataTemplateEngine: 'njk',
     htmlTemplateEngine: 'njk',
     templateFormats: ['md', 'njk', 'html', 'liquid'],
+=======
+module.exports = function(eleventyConfig) {
+  return {
+    templateFormats: [
+      "md",
+      "njk",
+      "html",
+      "liquid"
+    ],
+>>>>>>> Add: eleventy dependency
     // If your site lives in a different subdirectory, change this.
     // Leading or trailing slashes are all normalized away, so don’t worry about it.
     // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
     // This is only used for URLs (it does not affect your file structure)
+<<<<<<< HEAD
     pathPrefix: '/',
     passthroughFileCopy: true,
     dir: {
@@ -24,3 +36,16 @@ module.exports = function (eleventyConfig) {
     },
   };
 };
+=======
+    pathPrefix: "/",
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    passthroughFileCopy: true,
+    dir: {
+      input: "src",
+      output: "dist"
+    }
+  };
+};
+>>>>>>> Add: eleventy dependency
--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # cd-11ty
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 A personal website for Christina Dan and figuring out static site generators.
 =======
 Personal website for Christina Dan
 >>>>>>> Initial commit
+=======
+Personal website for Christina Dan
+=======
+
+A personal website for Christina Dan and figuring out static site generators.
+>>>>>>> Add: eleventy dependency
+>>>>>>> Add: eleventy dependency
